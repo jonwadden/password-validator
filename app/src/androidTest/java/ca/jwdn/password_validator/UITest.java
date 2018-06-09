f834671220450e93f1cda57cf3e72fe1a0a804d2
Jon Wadden <jon.wadden@dal.ca>
2018-06-09 14:45:35 -0300

    created UITest.java

---
+++ app/src/androidTest/java/ca/jwdn/password_validator/UITest.java
@@ -0,0 +1,18 @@
+package ca.jwdn.password_validator;
+
+
+
+import android.support.test.filters.LargeTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class UITest {
+    @Test
+    public void verifyInputDisplayed(){
+
+    }
+}
