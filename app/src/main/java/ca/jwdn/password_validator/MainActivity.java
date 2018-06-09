bd6d209e2784d30b183bf38012e200f17ea5d1dd
Jon Wadden <jon.wadden@dal.ca>
2018-06-09 16:39:16 -0300

    Completed UI functionality

--- app/src/main/java/ca/jwdn/password_validator/MainActivity.java
+++ app/src/main/java/ca/jwdn/password_validator/MainActivity.java
@@ -2,6 +2,9 @@ package ca.jwdn.password_validator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void submitButtonPressed(View view){
+        EditText passwordField = findViewById(R.id.passwordInput);
+        String passwordText = passwordField.getText().toString();
+        System.out.println(passwordText);
+        TextView resultLabel = findViewById(R.id.resultLabel);
+
+        Validator v = new Validator();
+        int testsPassed = v.validate(passwordText);
+        resultLabel.setText("Security: " + testsPassed + "/5");
+    }
+
+
 }
