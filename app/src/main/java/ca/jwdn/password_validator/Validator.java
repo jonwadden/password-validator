de7a778af2fe0d3d9ac841aeb708a17a128514ca
Jon Wadden <jon.wadden@dal.ca>
2018-06-09 14:21:57 -0300

    Implemented rules 1 & 2 in Validator.java

---
+++ app/src/main/java/ca/jwdn/password_validator/Validator.java
@@ -0,0 +1,32 @@
+package ca.jwdn.password_validator;
+
+public class Validator {
+    public Validator(){}
+
+    public int validate(String pw){
+        int testsPassed = 0;
+
+        /**
+         * Rules:
+         * (1 & 2 provided in assignment spec)
+         * 1 - is not 'password'
+         * 2 - at least 8 characters long
+         * 3 - contains at least 1 capital and 1 lowercase letter
+         * 4 - contains at least 1 number
+         * 5 - contains at least 1 non-alphanumeric character
+         */
+
+        // Test 1: Password is not 'password'
+        if (!pw.toLowerCase().equals("password")){
+            testsPassed++;
+        }
+
+        // Test 2: Password is at least 8 characters long
+        if (pw.length() >= 8){
+            testsPassed++;
+        }
+        
+        return testsPassed;
+    }
+
+}
